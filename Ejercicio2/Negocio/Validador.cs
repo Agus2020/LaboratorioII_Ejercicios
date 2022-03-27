d5ebcdc010930b3e7665668cdf2331293ed2e215
Agus2020 <aagustin067@gmail.com>
2022-03-26 23:52:51 -0300

    Ejercicio 2 incompleto

---
+++ Ejercicio2/Negocio/Validador.cs
@@ -0,0 +1,17 @@
+﻿namespace Negocio
+{
+    public class Validador
+    {
+        static public bool ValidarRespuesta(string opcion)
+        {
+            bool condicion = false;
+            if (opcion != 'S')
+            {
+                return condicion;
+            }
+            condicion = true;
+            return condicion;
+
+        }
+    }
+}
