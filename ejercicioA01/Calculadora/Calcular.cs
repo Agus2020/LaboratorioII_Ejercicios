2ea0010a387e1991c42d84c2651a7797f94ed8c9
Agus2020 <aagustin067@gmail.com>
2022-03-27 01:55:26 -0300

    ejercicioA01 terminado

---
+++ ejercicioA01/Calculadora/Calcular.cs
@@ -0,0 +1,17 @@
+﻿namespace Calculadora
+{
+    public class Calcular
+    {
+        static public int Factorizar (int numeroACalcular)
+        {
+            int factorial = 1;
+            int numFactorial = (int)numeroACalcular;
+            if (numFactorial != 0)
+            {
+                factorial = numFactorial * Factorizar(numFactorial - 1);
+            }
+            return factorial;
+        }
+
+    }
+}
---
+++ ejercicioA01/ejercicioA01/Program.cs
@@ -0,0 +1,9 @@
+﻿int valor;
+string valorString;
+bool resultado;
+int total;
+Console.WriteLine("Ingrese el numero a factorizar:");
+valorString = Console.ReadLine();
+resultado = int.TryParse(valorString,out valor);
+total=Calculadora.Calcular.Factorizar(valor);
+Console.WriteLine($"El resultado es {total}");
