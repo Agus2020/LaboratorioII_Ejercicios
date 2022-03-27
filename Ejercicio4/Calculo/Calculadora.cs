40365ea0a9e5c604ccd9690e489bcd9e23e1e14a
Agus2020 <aagustin067@gmail.com>
2022-03-27 01:00:01 -0300

    Ejercicio 4 terminado

---
+++ Ejercicio4/Calculo/Calculadora.cs
@@ -0,0 +1,47 @@
+﻿namespace Calculo
+{
+    public class Calculadora
+    {
+        public static float Calcular (int primerValor,int segundoValor,int operacion)
+        {
+            float resultado=2f;
+            bool resultadoValidar;
+            switch(operacion)
+            {
+                case 0:
+                    resultado =(float) primerValor + segundoValor;
+                    break;
+                case 1:
+                    resultado =(float) primerValor - segundoValor;
+                    break;
+                case 2:
+                    resultado =(float) primerValor * segundoValor;
+                    break;
+                case 3:
+                    resultadoValidar=Validar(segundoValor);
+                    if(resultadoValidar == true)
+                    {
+                        resultado =(float) primerValor / segundoValor;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Se ingreso un 0 en el segundo valor");
+                    }
+                    break;
+            }
+            return resultado;
+        }
+
+        private static bool Validar(int segundoValor)
+        {
+            bool resultado = false;
+            if (segundoValor != 0)
+            {
+                resultado = true;
+            }
+            return resultado;
+        }
+
+    }
+
+}
---
+++ Ejercicio4/Ejercicio4/Program.cs
@@ -0,0 +1,32 @@
+﻿int primerNumero;
+int segundoNumero;
+string primerNumeroTexto;
+string segundoNumeroTexto;
+bool resultadoPrimerNumero;
+bool resultadoSegundoNumero;
+int opcion=0;
+string opcionTexto;
+bool resultadoOpcion;
+int operacion;
+string operacionTexto;
+bool resultadoOperacion;
+float resultado=0F;
+do
+{
+    Console.WriteLine("Ingrese el primer numero:");
+    primerNumeroTexto = Console.ReadLine();
+    resultadoPrimerNumero = int.TryParse(primerNumeroTexto,out primerNumero);
+    Console.WriteLine("Ingrese el segundo numero:");
+    segundoNumeroTexto = Console.ReadLine();
+    resultadoSegundoNumero = int.TryParse(segundoNumeroTexto, out segundoNumero);
+    Console.WriteLine("Ingrese que operacion desea realizar: 0:Suma, 1:Resta, 2:Multiplicacion, 3:Division");
+    operacionTexto = Console.ReadLine();
+    resultadoOperacion = int.TryParse(operacionTexto,out operacion);
+    resultado = Calculo.Calculadora.Calcular(primerNumero,segundoNumero,operacion);
+    Console.WriteLine($"La operacion es {resultado}");
+    Console.WriteLine("Desea continuar? 1.Si, 2.No");
+    opcionTexto = Console.ReadLine();
+    resultadoOpcion = int.TryParse(opcionTexto,out opcion);
+
+}
+while (opcion==1);
