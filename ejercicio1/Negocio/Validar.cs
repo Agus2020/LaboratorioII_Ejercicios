4d514f7bc598b27263a82456f6b03776123d0aad
Agus2020 <aagustin067@gmail.com>
2022-03-26 22:57:52 -0300

    Primer ejercicio terminado

---
+++ ejercicio1/Negocio/Validar.cs
@@ -0,0 +1,16 @@
+﻿namespace Negocio
+{
+    public class Validar
+    {
+        static public bool Validador(int valor,int min,int max)
+        {
+            bool condicion;
+            condicion = false;
+            if(valor>=min && valor<=max)
+            {
+                condicion = true;
+            }
+            return condicion;
+        }
+    }
+}
---
+++ ejercicio1/ejercicio1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool resultado;
+            string numeroString;
+            int numero;
+            int min=0;
+            int max=0;
+            int acumulador=0;
+            float prom=0;
+            for (int i=0;i<10;i++)
+            {
+                do
+                {
+                    Console.WriteLine("Ingrese un numero:");
+                    numeroString = Console.ReadLine();
+                    resultado = int.TryParse(numeroString, out numero);
+                } 
+                while (Negocio.Validar.Validador(numero,-100,100)==false);
+                if(i==0)
+                {
+                    min = numero;
+                    max = numero;
+                    acumulador = numero;
+                }
+                else
+                {
+                    if(numero<min)
+                    {
+                        min = numero;
+                    }
+                    if(numero>max)
+                    {
+                        max = numero;
+                    }
+                    acumulador = acumulador + numero;
+                }
+            }
+            prom = acumulador / 10;
+            Console.WriteLine($"El numero maximo es {min}, el numero minimo es {max} y el promedio es {prom}");
+        }
+    }
+}
