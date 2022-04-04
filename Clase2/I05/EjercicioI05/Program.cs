2ad7d8ce367b60fc4c5b2d78cf588e7b2271d86f
Agus2020 <aagustin067@gmail.com>
2022-04-03 23:48:14 -0300

    Ejercicios terminados

---
+++ Clase2/I05/EjercicioI05/Program.cs
@@ -0,0 +1,8 @@
+﻿int numero;
+string numeroString;
+bool resultadoString;
+string textoInterpolado;
+Console.WriteLine("Ingrese un numero:");
+numeroString = Console.ReadLine();
+resultadoString = int.TryParse(numeroString,out numero);
+textoInterpolado = Negocio.Calculadora.Tabla(numero);
---
+++ Clase2/I08/Tiempo/TiempoPasa.cs
@@ -0,0 +1,46 @@
+﻿namespace Tiempo
+{
+    public class TiempoPasa
+    {
+        static public void Tiempo(int dia,int mes,int anio)
+        {
+
+            DateTime TiempoActual = new DateTime(2022, 03, 28);
+            DateTime Nacimiento = new DateTime(anio,mes,dia);
+            int diaActual = TiempoActual.Day;
+            int mesActual = TiempoActual.Day;
+            int anioActual = TiempoActual.Year;
+            int diaNacimiento = Nacimiento.Day;
+            int mesNacimiento = Nacimiento.Month;
+            int anioNacimiento = Nacimiento.Year;
+            int diaTotal;
+            int mesTotal;
+            int anioTotal;
+            if(diaActual>diaNacimiento)
+            {
+                diaTotal = diaActual - diaNacimiento;
+            }
+            else
+            {
+                diaTotal = diaNacimiento - diaActual;
+            }
+            if(mesActual>mesNacimiento)
+            {
+                mesTotal = mesActual - mesNacimiento;
+            }
+            else
+            {
+                mesTotal = mesNacimiento - mesActual;
+            }
+            if(anioActual>anioNacimiento)
+            {
+                anioTotal = anioActual - anioNacimiento;
+            }
+            else
+            {
+                anioTotal = anioNacimiento - anioActual;
+            }
+            Console.WriteLine($"Paso {anioTotal} anios, {mesTotal} meses y {diaTotal} dias");
+        }
+    }
+}
